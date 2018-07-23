b651d41cc7eb6da01a25afd2a763f9e758ab4628
Fabio Castilhos <fabcastilhos@gmail.com>
2018-07-23 20:35:20 -0300

    Artefatos para teste
    Fabio Castilhos

---
+++ config.js
@@ -0,0 +1,19 @@
+/*config.js*/
+
+/* Arquivo de Configuração */
+exports.config = {
+	seleniumAddress: 'http://localhost:8080/',
+	
+	specs: ['spec/*.js'],
+	
+	/* Iniciando o site desejado */
+	baseUrl: 'http://localhost:8080/index.php',
+	
+	capabilities: {
+		
+		browserName: 'chrome',
+		chromeOptions: {
+			args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
+		}
+	},	
+};
